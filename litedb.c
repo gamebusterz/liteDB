50ec3a309f6f2735b3f5c4de4537b97874133575
Sailesh <gamebusterz2@gmail.com>
2017-12-04 17:50:20 +0530

    Working basic prompt that exits

--- litedb.c
+++ litedb.c
@@ -1,3 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+#define true 1
+#define false 0
+
 struct InputBuffer_t {
   char* buffer;
   size_t buffer_length;
@@ -14,6 +21,22 @@ InputBuffer* new_input_buffer() {
 
   return input_buffer;
 }
+
+void print_prompt() {printf("db > ");}
+
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
 
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
