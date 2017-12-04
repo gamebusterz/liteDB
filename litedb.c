e99b312ff0428b045ce2ee03150f929eb11e2c2e
Sailesh <gamebusterz2@gmail.com>
2017-12-04 17:07:33 +0530

    First commit

---
+++ litedb.c
@@ -0,0 +1,32 @@
+struct InputBuffer_t {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+};
+
+typedef struct InputBuffer_t InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+int main(int argc, char* argv[]) {
+  InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    }
+  }
+}
+
+
